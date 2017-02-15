6517db5ca5933047227d6b1e15307ed400db8539
Mohammad Samiul Islam <forthright48@gmail.com>
2017-02-15 23:02:52 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+app.set('port', 8002);
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('*', function(req, res) {
+  return res.status(404).send('Page not found\n');
+});
+
+if (require.main === module) {
+  server.listen(app.get('port'), function() {
+    console.log(`Server running at port ${app.get('port')}`);
+  });
+} else {
+  module.exports = {
+    server,
+    app
+  };
+}
